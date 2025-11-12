cbe7f9a5baef4375cc4f80cd94123f2cdac146c3
Julio Grahl <144489686+JulioGrahl@users.noreply.github.com>
2025-11-12 09:36:50 -0300

    Update db.php

--- trabalhoFinal/src/db.php
+++ trabalhoFinal/src/db.php
@@ -1,9 +1,5 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_PORT', '5432');
-define('DB_NAME', 'trabalhoFinal');
-define('DB_USER', 'postgres');
-define('DB_PASS', 'postgres');
+require_once __DIR__ . '/../config.php'; 
 
 function conectar_db() {
     $conn_string = "host=" . DB_HOST . 
@@ -12,7 +8,7 @@ function conectar_db() {
                    " user=" . DB_USER . 
                    " password=" . DB_PASS;
     
-    $db_conn = pg_connect($conn_string);
+    $db_conn = @pg_connect($conn_string); 
 
     if (!$db_conn) {
         return false;
@@ -21,11 +17,5 @@ function conectar_db() {
 }
 
 $db = conectar_db();
-
-if (!$db) {
-    $db_error = true;
-} else {
-    $db_error = false;
-}
-
+$db_error = !$db;
 ?>
